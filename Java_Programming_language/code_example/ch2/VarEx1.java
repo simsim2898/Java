290d2aae1dace0c1cf752255f7b5ee7c569ebe39
simsim2898 <jehyun2898@gmail.com>
2024-07-08 19:31:21 +0900

    commit

---
+++ Java_Programming_language/code_example/ch2/VarEx1.java
@@ -0,0 +1,17 @@
+package Java_Programming_language.code_example.ch2;
+
+class VarEx1 {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}
